2a14680476c5630e7285a4f25f80eea5ecbaadcf
Pau-Blanco <paublaber@campus.monlau.com>
2025-02-24 18:55:51 +0100

    Commit Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
