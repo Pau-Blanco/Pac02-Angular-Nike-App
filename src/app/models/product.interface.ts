5a7321a6430afd6119ef8481ba19b1cf4e036c98
Pau-Blanco <paublaber@campus.monlau.com>
2025-02-25 19:32:28 +0100

    Creo el modelo product

---
+++ src/app/models/product.interface.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id?: number;
+    name: string;
+    serialNumber: string;
+    price: number;
+    description: string;
+    catergory: string;
+    stock: number;
+    image: string;
+}
