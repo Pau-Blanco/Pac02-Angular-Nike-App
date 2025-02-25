7c2003c1c13a2ab28f13e199586072414671e0ff
Pau-Blanco <paublaber@campus.monlau.com>
2025-02-25 17:44:03 +0100

    Añado el Model Productos y creo el formulario

---
+++ src/app/models/products.ts
@@ -0,0 +1,10 @@
+export interface Products
+{
+    reference: string,
+    name: string,
+    price: number,
+    description: string,
+    category: string,
+    sale: boolean,
+    image: string
+}
