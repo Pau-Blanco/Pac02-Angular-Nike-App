aabed7ef83149165ad670b081d38ddec9e69cdc4
Pau-Blanco <paublaber@campus.monlau.com>
2025-03-25 16:34:17 +0100

    Añado la logica del registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  registerForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  });
+
+  onSubmit(): void {
+    if (this.registerForm.invalid) return;
+
+    this.authService.register(this.registerForm.value).subscribe({
+      next: (response) => {
+        console.log('Registro exitoso: ', response);
+        this.router.navigate(['/login']); // Redirigir al Login
+      },
+      error: (error) => console.error('Error en el registro: ', error)
+    });
+  }
 }
