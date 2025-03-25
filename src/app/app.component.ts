b98ae2fc8eb40b0bb21854a29a8fce7e5ff10d4c
Pau-Blanco <paublaber@campus.monlau.com>
2025-03-25 16:49:45 +0100

    Añado el enrutamiento para que cuando abras el proyecto te redirija al login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
@@ -7,13 +7,21 @@ import { ProductosComponent } from './components/productos/productos.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeaderComponent, HeroComponent, ProductosComponent, AdminComponent, HomeComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, HeroComponent, ProductosComponent, AdminComponent, HomeComponent, FooterComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Nike-App';
+
+  constructor(private router: Router) { }
+
+  isAuthPage(): boolean {
+    return this.router.url.includes('/login') || this.router.url.includes('/register');
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductosComponent } from './components/productos/productos.component';
 import { AdminComponent } from './components/admin/admin.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
     {path: 'home', component: HomeComponent},
     {path: 'productos', component: ProductosComponent},
     {path: 'admin', component: AdminComponent}];
