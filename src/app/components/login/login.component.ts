6a8ccfe3644d6bb2e529271012a30e7bff0ad055
Pau-Blanco <paublaber@campus.monlau.com>
2025-03-25 16:18:09 +0100

    Añado la lógica del login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  });
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.log('Login exitoso: ', response);
+        this.router.navigate(['/home']); // Redirigir a Home
+      },
+      error: (error) => console.error('Error en login: ', error)
+    });
+  }
 }
