0126bed25db43ef7af27e29fc6fc7f52168d7dbf
Pau-Blanco <paublaber@campus.monlau.com>
2025-03-26 18:55:37 +0100

    creo el servicio para conectarnos a la API

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'http://localhost:3000/api/users';  // URL de los endpoints de usuarios
+
+  constructor(private http: HttpClient) { }
+
+  // Obtener información del usuario autenticado
+  getUserInfo(): Observable<any> {
+    const token = localStorage.getItem('authToken');
+    if (!token) {
+      console.error('No hay token, acceso denegado.');
+      return new Observable(); // Evita hacer la petición si no hay token
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get(`${this.apiUrl}/me`, { headers });
+  }
+
+  // Actualizar información del usuario autenticado
+  updateUserInfo(updatedUserData: any): Observable<any> {
+    const token = localStorage.getItem('authToken');
+    if (!token) {
+      console.error('No hay token, acceso denegado.');
+      return new Observable(); // Evita hacer la petición si no hay token
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put(`${this.apiUrl}/update`, updatedUserData, { headers });
+  }
+}
