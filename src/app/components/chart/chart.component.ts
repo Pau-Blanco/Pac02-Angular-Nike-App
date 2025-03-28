ab1bb007e754db4a6b8bdf7d8d3d9f11a45959bd
Pau-Blanco <paublaber@campus.monlau.com>
2025-03-28 16:45:11 +0100

    Creo Servicio para realizar pedidos y hago cambios en el carrito

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartService } from '../../services/chart.service';
 import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product.interface';
 
 @Component({
   selector: 'app-chart',
@@ -10,8 +11,9 @@ import { CommonModule } from '@angular/common';
 })
 export class ChartComponent implements OnInit {
   cartItems: any[] = [];
+  totalAmount: number = 0;
 
-  constructor(private chartService: ChartService) { }
+  constructor(private chartService: ChartService) {}
 
   ngOnInit(): void {
     this.getCart();
@@ -19,22 +21,34 @@ export class ChartComponent implements OnInit {
 
   getCart(): void {
     this.chartService.getCart().subscribe(
-      (data) => this.cartItems = data.items,
+      (data) => {
+        this.cartItems = data.items;
+        this.calculateTotal();
+      },
       (error) => console.error('Error al obtener el carrito', error)
     );
   }
 
-  removeFromCart(product_id: number): void {
-    this.chartService.removeFromCart(product_id).subscribe(
-      () => this.getCart(),
+  removeFromCart(productId: number): void {
+    this.chartService.removeFromCart(productId).subscribe(
+      () => {
+        this.getCart();
+      },
       (error) => console.error('Error al eliminar producto', error)
     );
   }
 
   clearCart(): void {
     this.chartService.clearCart().subscribe(
-      () => this.cartItems = [],
+      () => {
+        this.cartItems = [];
+        this.totalAmount = 0;
+      },
       (error) => console.error('Error al vaciar el carrito', error)
     );
+  }
+
+  calculateTotal(): void {
+    this.totalAmount = this.cartItems.reduce((sum, item) => sum + (item.price * item.quantity), 0);
   }
 }
